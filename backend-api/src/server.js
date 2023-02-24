b68fdf37531e76235e078952818e1c50fb6186f0
Patrick Henegan <phenegan03@gmail.com>
2023-02-23 21:37:50 -0500

    updated the backend with new requirements (minus session tracking)

--- backend-api/src/server.js
+++ backend-api/src/server.js
@@ -1,5 +1,8 @@
 import Express from "express";
-import * as users from "./users.js";
+import PokemonDatabase from "./users.js";
+
+
+const db = await PokemonDatabase.fromFile('./src/db/pokemon-teams.sqlite');
 
 const app = Express();
 
@@ -7,37 +10,102 @@ const port = process.env.PORT || 8080;
 
 app.use(Express.json());
 
-// Gets the team for a specific username
-app.get("/team/:id", (req, res) => {
-  console.log(`getting team from ${req.params.id}`);
+// Attempts to create a new user with the given name and password
+app.post("/user", async (req, res) => {
   try {
-    const team = users.getTeam(req.params.id);
+    db.addUser(req.body.username, req.body.password);
+    res.send();
+  } catch (e) {
+    res.status(400).send(`Username already exists`);
+  }
+});
+
+// Attempts to update a user's password
+app.put("/user", async (req, res) => {
+  try {
+    db.changePassword(req.body.user, req.body.oldPassword, req.body.newPassword);
+  } catch (e) {
+    res.status(403).send();
+  }
+});
+
+// Attempts to remove a given user
+app.delete("/user", async (req, res) => {
+  try {
+    db.removeUser(req.body.user, req.body.password);
+  } catch (e) {
+    res.status(403).send();
+  }
+})
+
+// Create a new team with the given name
+app.post("/team/:user/:teamName", (req, res) => {
+  db.addTeam(req.params.user, req.params.teamName);
+});
+
+// Gets the team for a specific username and team name
+app.get("/team/:user/:teamName", async (req, res) => {
+  console.log(`getting team '${req.params.teamName}' from '${req.params.user}'`);
+  try {
+    const team = await db.getTeam(req.params.user, req.params.teamName);
     console.log(team);
     res.send(team);
   } catch (e) {
-    res.status(400).send(`Username ${req.params.id} does not exist`);
+    console.log(e);
+    res.status(400).send(`Username ${req.params.user} or Team ${req.params.teamName} does not exist`);
   }
 });
 
-// Updates the team for a specific username
-app.put("/teams", (req, res) => {
-  console.log("update request received");
-
+// remove a team from a given user
+app.delete("/team/:user/:teamName", async (req, res) => {
   try {
-    users.updateUser(req.body.name, req.body.team, req.body.numAdded);
-    res.send("updated team");
+    db.removeTeam(req.params.user, req.params.teamName);
   } catch (e) {
-    console.log("could not find user")
-    res.status(400).send(`Username ${req.body.name} does not exist`);
+    console.log(e);
+    res.status(400).send(`Username ${req.params.user} does not have a team ${req.params.teamName}`)
   }
 });
 
-// Attempts to create a new user with the given name
-app.post("/users/:id", (req, res) => {
-  const success = users.addUser(req.params.id);
-  console.log(`requested to make new user ${req.params.id}, resulting in ${success}`);
-  res.send(success);
+// Get the list of teams for a specific username
+app.get("/teams/:user", async (req, res) => {
+  try {
+    const teamList = await db.getUserTeams(req.params.user);
+    console.log(teamList);
+    res.send(teamList);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(`Username ${req.params.user} does not exist`);
+  }
 });
+
+// Adds a new Pokemon with the given pokemon name and nickname to the given team
+app.post("/pokemon/:user/:teamName", async (req, res) => {
+  try {
+    const pID = await db.addPokemon(req.params.user, req.params.teamName, req.body)
+    res.send(pID);
+  } catch (e) {
+    res.status(400).send(`Username ${req.params.user} or Team ${req.params.teamName} does not exist`);
+  }
+});
+
+// Updates the pokemon with the given ID on the given team
+app.put("/pokemon/:user/:teamName:", async (req, res) => {
+  try {
+    await db.updatePokemon(req.params.user, req.params.teamName, req.body);
+  } catch (e) {
+    res.status(400).send(`Invalid Inputs`);
+  }
+});
+
+// Removes a Pokemon with the given ID from the database
+app.delete("/pokemon/:user/:pokemonID", async (req, res) => {
+  try {
+    await db.removePokemonByID(req.params.pokemonID);
+  } catch (e) {
+    res.status(400).send(`Invalid Pokemon ID: ${req.params.pokemonID}`)
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
--- backend-api/src/users.js
+++ backend-api/src/users.js
@@ -1,63 +1,154 @@
-const users = [];
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+export default class PokemonDatabase {
+  constructor(db) {
+    this.db = db;
+  }
+
+  static async fromFile(filename) {
+    const db = open({
+      filename: filename,
+      driver: sqlite3.Database,
+    });
+    return new PokemonDatabase(await db);
+  }
+
+  /**
+   * Adds a new user with the given team to the list
+   *
+   * @param {string} username - the username of the user being created
+   * @param {string} password - the (hopefully hashed) password of the user being created
+   */
+  async addUser(username, password) {
+    try {
+      await this.db.run(
+        `INSERT INTO Users (username, password) VALUES ('${username}', '${password}');`
+      );
+    } catch (e) {
+      console.log(e);
+      throw new Error(`username ${username} already exists`);
+    }
+  }
+
+  /**
+   * Updates the user's password to the new password
+   *
+   * @param {string} username
+   * @param {string} oldPassword
+   * @param {string} newPassword
+   */
+  async changePassword(username, oldPassword, newPassword) {
+    await this.db.run(
+      `UPDATE Users SET password = '${newPassword}' ` +
+        `WHERE username = '${username}' AND password = '${oldPassword}';`
+    );
+  }
+
+  /**
+   * Removes the user with the given username and password
+   * @param {string} username the user to remove
+   * @param {string} password password used for authentication purposes before deleting
+   */
+  async removeUser(username, password) {
+    await this.db.run(
+      `DELETE FROM Users WHERE username = '${username}' AND password = '${password}'`
+    );
+  }
+
+  /**
+   * Gets all of the teams from the given user
+   * @param {string} username - the name of the user being accessed
+   * @returns a list of teams and their ids
+   */
+  async getUserTeams(username) {
+    const teams = await this.db.all(
+      `SELECT teamName FROM Users ` +
+        `JOIN Teams ON Users.username = Teams.userID ` +
+        `WHERE username = '${username}';`
+    );
+    console.log(teams);
+    return teams;
+  }
+
+  /**
+   * Adds a new team under the given user with the given name
+   * @param {string} username
+   * @param {string} teamName
+   */
+  async addTeam(username, teamName) {
+    this.db.run(`INSERT INTO Teams (username, teamName) VALUES ('${username}', '${teamName}');`);
+  }
+
+  /**
+   * Adds the given pokemon to the database under the given team from the given user
+   *
+   * @param {string} username the user adding the pokemon
+   * @param {string} teamName the team the pokemon is being added to
+   * @param {object} pokemon the object representing the pokemon
+   * @returns the id of the newly created pokemon
+   */
+  async addPokemon(username, teamName, pokemon) {
+    const teamID = await findTeamIDByName(this.db, username, teamName);
+    await this.db.run(
+      `INSERT INTO Pokemon (teamID, name, nickname) VALUES (${teamID}, '${pokemon.name}', '${pokemon.nickname}');`
+    );
+    const id = this.db.get("SELECT LAST_INSERT_ROWID() AS id;");
+    return id;
+  }
+
+  async updatePokemon(username, teamName, pokemon) {
+    const teamID = await findTeamIDByName(this.db, username, teamName);
+    await this.db.run(
+      `UPDATE Pokemon SET name='${pokemon.name}', nickname='${pokemon.nickname}' ` +
+        `WHERE teamName = '${teamID}' AND pokemonID = ${pokemon.id};`
+    );
+  }
+
+  /**
+   * removes the pokemon with the given ID
+   * @param {number} pokemonID
+   */
+  async removePokemonByID(pokemonID) {
+    this.db.run(`DELETE FROM Pokemon WHERE pokemonID = ${pokemonID};`);
+  }
+
+  /**
+   * Finds the team from a given username and team name
+   * @param {string} username
+   * @returns the array of objects representing a team
+   */
+  async getTeam(username, teamName) {
+    const statement =
+      `SELECT name, nickname, pokemonID ` +
+      `FROM Pokemon JOIN Teams ON Pokemon.teamID = Teams.teamID ` +
+      `WHERE userID = '${username}' AND teamName = '${teamName}';`;
+    console.log(statement);
+    const team = this.db.all(statement);
+    return team;
+  }
+
+  /**
+   * Removes the team and all pokemon in the user's given team from the database
+   * @param {string} username the name of the user removing a team
+   * @param {string} teamName the name of the team to remove
+   */
+  async removeTeam(username, teamName) {
+    const teamID = findTeamIDByName(this.db, username, teamName);
+    this.db.run(`DELETE FROM Pokemon WHERE teamID = ${teamID}`);
+    this.db.run(`DELETE FROM Teams WHERE teamID = ${teamID}`);
+  }
+}
 
 /**
- * Adds the a new user with the given team to the list
+ * Tries to find the ID of a team with the given name from the given user
  *
- * @param {string} username
- * @param {*} team
+ * @param {string} username - the username of the user being created
+ * @param {string} teamName - the password to compare against the database
  */
-export function addUser(username) {
-  if (users.find(eachUser => eachUser.name === username)) {
-    return false;
-  }
-  
-    const newUser = {
-    name: username,
-    numAdded: 0,
-    team: [],
-  };
-
-  users.push(newUser);
-  return true;
-}
-
-/**
- * Updates the team of an existing user in the list
- *
- * @param {string} username
- * @param {*} team
- */
-export function updateUser(username, team, numAdded) {
-  const user = users.find((eachUser) => eachUser.name === username);
-  if (!user) {
-    throw new Error(`Unable to find user matching name ${username}`);
-  }
-  user.team = team;
-  user.numAdded = numAdded;
-}
-
-/**
- * Finds the team (and all other user data) from a given username
- * @param {string} username 
- * @returns the array of objects representing a team
- */
-export function getTeam(username) {
-  const user = findUserInList(username);
-  console.log(user);
-  return user;
-}
-
-/**
- * Finds the user object given the user's name
- * @param {string} username 
- * @returns the user object corresponding to that username
- */
-function findUserInList(username) {
-  const user = users.find((eachUser) => eachUser.name === username);
-
-  if (!user) {
-    throw new Error(`Unable to find user matching name ${username}`);
-  }
-
-  return user;
+async function findTeamIDByName(db, username, teamName) {
+  const teamID = db.get(
+    `SELECT teamID FROM Teams WHERE username = '${username}' AND teamName = '${teamName}';`
+  );
+  return teamID;
 }
